774aab03bdce0c3cc9e922cf9ad8d6963b100deb
nina88c <131000810+nina88c@users.noreply.github.com>
2023-11-13 20:14:55 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="App"> Weather App</div>
+    <div className="App">
+      <div className="container">
+        <p>Weather App</p>
+        <button className="btn btn-rimary" >hello</button>
+        <a href="https://www.shecodes.io/learn/workshops/1359/units/75/lessons/197">
+          Click me!
+        </a>
+      </div>
+    </div>
   );
 }
